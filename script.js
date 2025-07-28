8865e5e6fa281c191d8311e25e0e7941c3d832be
dangardiner90 <danieljosephgardiner@gmail.com>
2025-07-28 08:41:17 -0400

    Adding mouse event functionality

---
+++ script.js
@@ -0,0 +1,11 @@
+// Part 1: Mouse events
+
+const mouseEvent = document.getElementById('mouseEvent');
+// I decided to make the event occur when the mouse is hovered over
+mouseEvent.addEventListener('mouseenter', function() {
+    mouseEvent.textContent = "I am being hovered over";
+});
+// The message also changes once it leaves the button area as well
+mouseEvent.addEventListener('mouseleave', function() {
+    mouseEvent.textContent = "I was hovered over";
+});
